521d0513b1425de860041c82ac5e43a9cbe686d8
PatrickKR <jehyeokchoe@gmail.com>
2020-09-24 13:04:43 +0900

    feat: enhanced paper launch options

--- main/main.js
+++ main/main.js
@@ -9,6 +9,7 @@ const readline = require('readline');
 const buildDir = checkDir(path.join(__dirname, 'build'));
 const jarsDir = checkDir(path.join(buildDir, 'jars'));
 const serversDir = checkDir(path.join(buildDir, 'servers'));
+const cacheDir = checkDir(path.join(jarsDir, 'cache'));
 
 function checkDir(dir) {
     if (!fs.existsSync(dir)) {
@@ -36,9 +37,9 @@ async function downloadPaper(version) {
         } catch(ignored) {}
         const time = response.headers['last-modified'];
         if (time == last) {
-            console.log(`Paper ${version} already exists...`)
+            console.log(`Latest paper ${version} already exists...`);
         } else {
-            console.log(`Paper ${version} downloading...`)
+            console.log(`Downloading paper ${version}...`);
             fs.writeFile(log, time, (error) => {
                 if (error) {
                     flag = false;
@@ -55,46 +56,74 @@ async function downloadPaper(version) {
     return new Promise((resolve, reject) => {
         if (typeof stream !== 'undefined' && stream) {
             stream.on('close', () => {
-                if (flag == true) {
-                    resolve()
-                } else {
-                    reject()
-                }
+                console.log(`Patching paper ${version}...`);
+                const child = child_process.exec(`java -Dpaperclip.patchonly=true -jar ${jar}`, {
+                    cwd: jarsDir
+                });
+                child.on('exit', () => {
+                    fs.unlink(path.join(cacheDir, `mojang_${version}.jar`), (error) => {
+                        if (error) {
+                            flag = false;
+                            console.log(error);
+                        }
+                        if (flag == true) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                })
             })
         } else if (flag == true) {
-            resolve()
+            resolve();
         } else {
-            reject()
+            reject();
         }
     });
 }
 
+function startPaper(name, version, syncWithProcess) {
+    downloadPaper(version).then(() => {
+        const jar = path.join(jarsDir, `paper_${version}.jar`);
+        const server = checkDir(path.join(serversDir, `paper_${version}_${name}`));
+        const cache = checkDir(path.join(server, "cache"));
+
+        fs.copyFileSync(path.join(cacheDir, `patched_${version}.jar`), path.join(cache, `patched_${version}.jar`));
+
+        console.log(`Starting paper ${version} (Server name: ${name})...`);
+        const child = child_process.exec("java -Xms4G -Xmx4G -Xmn3G " +
+            "-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 " +
+            "-XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch " +
+            "-XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 " +
+            "-XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 " +
+            "-XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 " +
+            "-XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 " + 
+            "-Dfile.encoding=UTF-8 -Dcom.mojang.eula.agree=true " + 
+            "-Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true " + 
+            `-jar ${jar} --world-dir worlds/ nogui`, {
+            cwd: server
+        });
+
+        if (syncWithProcess) {
+            child.stdout.pipe(process.stdout)
+            const readInterface = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout,
+                terminal: false
+            });
+            readInterface.on('line', function (line) {
+                child.stdin.write(`${line}\n`);
+            });
+            child.on('exit', () => {
+                process.exit();
+            });
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
 const name = "Nitro";
-const version = "1.15.2";
+const version = "1.12.2";
 
-downloadPaper(version).then(() => {
-    console.log(`Paper ${version} download success`);
-
-    const jar = path.join(jarsDir, `paper_${version}.jar`);
-    const server = checkDir(path.join(serversDir, name));
-
-    console.log(jar);
-    console.log(server);
-    const child = child_process.exec(`java -Xms4G -Xmx4G -Xmn3G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dfile.encoding=UTF-8 -Dcom.mojang.eula.agree=true -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar ${jar} --world-dir worlds/ nogui`, {
-        cwd: server
-    });
-    child.stdout.pipe(process.stdout)
-    const readInterface = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        terminal: false
-    });
-    readInterface.on('line', function (line) {
-        child.stdin.write(`${line}\n`);
-    });
-    child.on('exit', function () {
-        process.exit()
-    });
-}).catch(() => {
-    console.error("failed");
-});
+startPaper(name, version, true);
